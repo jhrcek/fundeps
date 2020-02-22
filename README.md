8042497ae29b63a381364e06cf225c9adc6547ed
Jan Hrček <jhrcek@globalwebindex.com>
2020-02-22 10:56:04 +0100

    elm-stuff deletion no longer needed after elm hack improved

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # fundeps
 
 An experimental CLI tool for visualization of elm declaration dependencies.
+This tool depends on [graphviz](https://www.graphviz.org/) being installed on your system.
 
 # TODOs
 * Make the autocompletion work for queries of the form `Module:function` even for modules from external packages
 * Ask for confirmation when displaying too large graphs
+* Find better way how to integrate hacked elm compiler into the tool
--- fundeps.hs
+++ fundeps.hs
@@ -7,7 +7,6 @@
 
 {-# LANGUAGE OverloadedStrings #-}
 
-import Control.Exception
 import System.Process
 import Turtle
 
@@ -17,8 +16,7 @@ main = do
   elmJsonExists <- testfile "elm.json"
   unless elmJsonExists $
     die "elm.json not found in current directory. This doesn't look like elm project."
-  withElmStuffBackup $
-    procs "hacked-elm" ("make" : "--output=/dev/null" : filesToCompile) empty
+  procs "hacked-elm" ("make" : "--output=/dev/null" : filesToCompile) empty
   output "tmp" $ do
     usagesFile <- getUsages
     input usagesFile
@@ -26,21 +24,6 @@ main = do
   mv "tmp" "all.usages"
   sh $ liftIO $ callProcess "fundeps-exe" []
   rm "all.usages"
-
-withElmStuffBackup :: IO () -> IO ()
-withElmStuffBackup action = do
-  elmStuffExists <- testdir stuff
-  if elmStuffExists
-    then withBackup action
-    else action
-  where
-    withBackup act = with (mktempdir "/tmp" "fundeps") $ \tmpDir ->
-      bracket_
-        -- TODO figure out how to do it via `mv`, which should be much faster
-        -- TODO or better yet hack elm so it completely ignores local project cache and always recompiles the files
-        (cptree stuff (tmpDir </> stuff) >> rmtree stuff)
-        (rmtree stuff >> cptree (tmpDir </> stuff) stuff)
-        act
 
 getUsages :: Shell Turtle.FilePath
 getUsages = mfilter (\f -> extension f == Just "usages") (ls ".")
@@ -50,6 +33,3 @@ parseArgs =
   options "Script to run fundeps"
     $ some
     $ argText "FILE(s)" "One or more elm files to compile"
-
-stuff :: Turtle.FilePath
-stuff = "elm-stuff/"
